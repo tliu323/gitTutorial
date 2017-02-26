f95d3470d033a59d20c00801b8bb6c752f4676d8
vince <tliu323@uwo.ca>
2017-02-26 13:53:31 -0500

    added course class and made changes to student and instructor

---
+++ src/Course.java
@@ -0,0 +1,12 @@
+/**
+ * Created by vince on 2017-02-26.
+ */
+public class Course {
+    String cname;
+    String description;
+
+    public Course(String cname, String description){
+        this.cname = cname;
+        this.description = description;
+    }
+}
